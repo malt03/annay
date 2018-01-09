31ac853b0453b13597a1bc274a307b4b7360f1ee
Koji Murata <koji.murata@dena.com>
2018-01-09 21:32:50 +0900

    Initial Commit

---
+++ MarkDownEditor/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MarkDownEditor
+//
+//  Created by Koji Murata on 2018/01/09.
+//  Copyright © 2018年 Koji Murata. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
