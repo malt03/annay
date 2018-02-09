da96f321907f2ac09f464c6471f88d8b4c407493
Koji Murata <koji.murata@dena.com>
2018-02-09 18:27:56 +0900

    謎バグ

--- MarkDownEditor/Common/BackgroundSetableView.swift
+++ MarkDownEditor/Common/BackgroundSetableView.swift
@@ -16,11 +16,18 @@ class BackgroundSetableView: NSView {
   @IBInspectable var borderColor: NSColor?
 
   override func draw(_ dirtyRect: NSRect) {
-    backgroundColor?.setFill()
-    borderColor?.setStroke()
-    let path = NSBezierPath(roundedRect: dirtyRect, xRadius: cornerRadius, yRadius: cornerRadius)
-    path.fill()
-    path.stroke()
+    var flag = false
+    if let backgroundColor = backgroundColor {
+      flag = true
+      backgroundColor.setFill()
+    }
+    if let borderColor = borderColor {
+      flag = true
+      borderColor.setStroke()
+    }
+    if flag {
+      NSBezierPath(roundedRect: dirtyRect, xRadius: cornerRadius, yRadius: cornerRadius).fill()
+    }
 
     super.draw(dirtyRect)
   }
