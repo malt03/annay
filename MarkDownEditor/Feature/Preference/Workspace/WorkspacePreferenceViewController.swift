284f1b3ac983f3c0eba31571f06528e510163d38
Koji Murata <koji.murata@dena.com>
2018-04-13 15:24:08 +0900

    でけた

--- MarkDownEditor/Feature/Preference/Workspace/WorkspacePreferenceViewController.swift
+++ MarkDownEditor/Feature/Preference/Workspace/WorkspacePreferenceViewController.swift
@@ -12,14 +12,35 @@ import RxSwift
 final class WorkspacePreferenceViewController: NSViewController {
   private let bag = DisposeBag()
   
+  private let selectedWorkspace = Variable(WorkspaceModel.selectedValue)
+  
   @IBOutlet private weak var workspacePopUpButton: NSPopUpButton!
+  @IBOutlet private weak var fileTypePopUpButton: NSPopUpButton!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    WorkspaceModel.observableSpaces.subscribe(onNext: { [weak self] (spaces) in
+    WorkspaceModel.observableSpaces.distinctUntilChanged().subscribe(onNext: { [weak self] (spaces) in
       guard let s = self else { return }
       s.workspacePopUpButton.removeAllItems()
       s.workspacePopUpButton.addItems(withTitles: spaces.map { $0.nameValue })
     }).disposed(by: bag)
+    
+    workspacePopUpButton.rx.controlEvent.subscribe(onNext: { [weak self] _ in
+      guard let s = self else { return }
+      s.selectedWorkspace.value = WorkspaceModel.spaces[s.workspacePopUpButton.indexOfSelectedItem]
+    }).disposed(by: bag)
+
+    selectedWorkspace.asObservable().subscribe(onNext: { [weak self] (workspace) in
+      guard let s = self else { return }
+      let index = workspace.isFolderUrl ? 1 : 0
+      s.fileTypePopUpButton.selectItem(at: index)
+    }).disposed(by: bag)
+
+    fileTypePopUpButton.rx.controlEvent.subscribe(onNext: { [weak self] _ in
+      guard let s = self else { return }
+      alertError {
+        try s.selectedWorkspace.value.setIsFolderUrl(s.fileTypePopUpButton.indexOfSelectedItem == 1)
+      }
+    }).disposed(by: bag)
   }
-  
 }
