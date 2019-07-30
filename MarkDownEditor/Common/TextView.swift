5a3f002f65d6e34b22bc3799ad28777759325a57
Koji Murata <koji.murata@dena.com>
2019-07-30 10:26:20 +0900

    toggleをもうちょっと良さげに

--- MarkDownEditor/Common/TextView.swift
+++ MarkDownEditor/Common/TextView.swift
@@ -26,24 +26,16 @@ final class TextView: NSTextView {
   @objc private func strikethrough(_ sender: Any) { toggleFormat(Format(symbol: "~~")) }
   private func toggleFormat(_ format: Format) {
     let range = selectedRange()
-    var isRemoveFormat = false
     
-    let symbolLength = format.symbol.count
-    let beforeSymbolRange = NSRange(location: range.lowerBound - symbolLength, length: symbolLength)
-    let afterSymbolRange = NSRange(location: range.upperBound, length: symbolLength)
-    if beforeSymbolRange.lowerBound >= 0 && afterSymbolRange.upperBound <= (string as NSString).length {
-        let beforeSymbol = (string as NSString).substring(with: beforeSymbolRange)
-        let afterSymbol = (string as NSString).substring(with: afterSymbolRange)
-        if beforeSymbol == format.symbol && afterSymbol == format.symbol {
-            isRemoveFormat = true
-        }
-    }
-    
-    if isRemoveFormat {
-        let newText = (string as NSString).substring(with: range)
-        let rangeWithSymbol = NSRange(location: beforeSymbolRange.lowerBound, length: afterSymbolRange.upperBound - beforeSymbolRange.lowerBound)
-        insertText(newText, replacementRange: rangeWithSymbol)
-        setSelectedRange(NSRange(location: beforeSymbolRange.lowerBound, length: range.length))
+    if
+      let beforeSymbolRange = searchSymbol(before: true, selectedRange: range, symbol: format.symbol),
+      let afterSymbolRange = searchSymbol(before: false, selectedRange: range, symbol: format.symbol)
+    {
+      let newTextRange = NSRange(location: beforeSymbolRange.upperBound, length: afterSymbolRange.lowerBound - beforeSymbolRange.upperBound)
+      let newText = (string as NSString).substring(with: newTextRange)
+      let rangeWithSymbol = NSRange(location: beforeSymbolRange.lowerBound, length: afterSymbolRange.upperBound - beforeSymbolRange.lowerBound)
+      insertText(newText, replacementRange: rangeWithSymbol)
+      setSelectedRange(NSRange(location: beforeSymbolRange.lowerBound, length: newTextRange.length))
     } else {
         insertText(format.symbol, replacementRange: NSRange(location: range.upperBound, length: 0))
         insertText(format.symbol, replacementRange: NSRange(location: range.lowerBound, length: 0))
@@ -51,6 +43,26 @@ final class TextView: NSTextView {
             setSelectedRange(NSRange(location: range.location + format.symbol.count, length: 0))
         }
     }
+  }
+  
+  private func searchSymbol(before: Bool, selectedRange: NSRange, symbol: String) -> NSRange? {
+    let beginningOfCheckRanges: [Int]
+    if before {
+      beginningOfCheckRanges = [Int]((selectedRange.lowerBound - symbol.count)...selectedRange.lowerBound)
+    } else {
+      beginningOfCheckRanges = [Int]((selectedRange.upperBound - symbol.count)...selectedRange.upperBound).reversed()
+    }
+    
+    for beginningOfCheckRange in beginningOfCheckRanges {
+      let checkRange = NSRange(location: beginningOfCheckRange, length: symbol.count)
+      
+      if checkRange.lowerBound >= 0 && checkRange.upperBound <= (string as NSString).length {
+        if (string as NSString).substring(with: checkRange) == symbol {
+          return checkRange
+        }
+      }
+    }
+    return nil
   }
   
   override func awakeFromNib() {
